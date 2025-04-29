c607fec731b69f18d2797df1c1ef0fcdc3dc12e3
Adam GONCALVES <goncalvesadam@icloud.com>
2025-04-29 21:40:04 +0200

    next.js init & conf: react, tailwind, eslint...

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,9 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import tailwind from 'eslint-plugin-tailwindcss';
+import js from '@eslint/js';
+import ts from 'typescript-eslint';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,8 +12,6 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+const eslintConfig = [js.configs.recommended, ...ts.configs.recommended, ...compat.extends('next/core-web-vitals', 'next/typescript'), ...tailwind.configs['flat/recommended']];
 
 export default eslintConfig;
