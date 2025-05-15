28fb43dab773f618ead2dac86676f58216267670
Adam GONCALVES <goncalvesadam@icloud.com>
2025-05-15 07:22:16 +0200

    fix Hero & added Status component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -10,7 +10,7 @@ const Code = ({ className, ...props }: ComponentPropsWithoutRef<"span">) => {
 export const Hero = () => {
   return (
     <Section className="flex max-md:flex-col items-start gap-8">
-      <div className="flex-[3] flex flex-col gap-2">
+      <div className="flex-[2] flex flex-col gap-2">
         <h2 className="font-caption text-4xl">Adam GONÇALVES</h2>
         <h3 className="font-caption text-2xl">Full-Stack developer</h3>
         <p>
@@ -18,7 +18,7 @@ export const Hero = () => {
           Passionné par l&apos;informatique et en pleine formation en développement d&apos;applications et web, je maîtrise les bases de la programmation et aime relever des défis techniques. Curieux et motivé, j&apos;apprends rapidement et cherche à développer des solutions innovantes. Mon objectif ? Allier créativité et performance pour concevoir des applications efficaces et ergonomiques.
         </p>
       </div>
-      <div className="flex-[2] max-md:m-auto ml-auto">
+      <div className="flex-1 max-md:m-auto ml-auto">
         <img src="adam-picture.jpeg" className="w-full h-auto max-w-sm rounded-full max-md:w-56" alt="adam's picture" />
       </div>
     </Section>
